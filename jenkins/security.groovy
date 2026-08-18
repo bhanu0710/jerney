4d758367281be57ff33ab9f5c3f30d679a58cca9
bhanu0710 <157271963+bhanu0710@users.noreply.github.com>
2026-08-18 23:17:19 +0530

    Update security.groovy

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -1,17 +1,33 @@
 def filesystemScan(String path) {
+
     sh """
+    mkdir -p reports
+
     trivy fs \
-    --severity HIGH,CRITICAL \
-    ${path}
+      --severity HIGH,CRITICAL \
+      --format table \
+      --output reports/${path}-trivy-fs.txt \
+      ${path}
     """
+
 }
 
 def imageScan(String image) {
+
+    def reportName = image
+            .replace("/", "-")
+            .replace(":", "-")
+
     sh """
+    mkdir -p reports
+
     trivy image \
-    --severity HIGH,CRITICAL \
-    ${image}
+      --severity HIGH,CRITICAL \
+      --format table \
+      --output reports/${reportName}.txt \
+      ${image}
     """
+
 }
 
 return this
