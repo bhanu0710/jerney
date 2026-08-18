2eb7117a916cd04f0f7d0707cdf2ca1c7bf8323b
bhanu0710 <157271963+bhanu0710@users.noreply.github.com>
2026-08-18 22:53:14 +0530

    Update utils.groovy

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -1,9 +1,44 @@
-def gitSha(){
+def gitSha() {
 
     return sh(
-        script:'git rev-parse --short HEAD',
-        returnStdout:true
+        script: "git rev-parse --short HEAD",
+        returnStdout: true
     ).trim()
+
+}
+
+def changedFiles() {
+
+    return sh(
+        script: """
+        git diff --name-only HEAD~1 HEAD
+        """,
+        returnStdout: true
+    ).trim().split("\n")
+
+}
+
+def backendChanged(files) {
+
+    return files.any { file ->
+        file.startsWith("backend/")
+    }
+
+}
+
+def frontendChanged(files) {
+
+    return files.any { file ->
+        file.startsWith("frontend/")
+    }
+
+}
+
+def manifestsChanged(files) {
+
+    return files.any { file ->
+        file.startsWith("k8s/")
+    }
 
 }
 
