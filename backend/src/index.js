6a632430f5b970f463ecc2807e58297c264388fa
bhanu0710 <157271963+bhanu0710@users.noreply.github.com>
2026-08-18 13:51:46 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const postRoutes = require('./routes/posts');
 const commentRoutes = require('./routes/comments');
 const db = require('./db');
+const {
+    client,
+    httpRequests,
+    httpDuration
+} = require("./metrics");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,14 +17,46 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req,res,next)=>{
+
+    const end = httpDuration.startTimer();
+
+    res.on("finish",()=>{
+
+        httpRequests.inc({
+            method:req.method,
+            route:req.path,
+            status:res.statusCode
+        });
+
+        end({
+            method:req.method,
+            route:req.path,
+            status:res.statusCode
+        });
+
+    });
+
+    next();
+
+});
+
 // Health check
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', message: 'Jerney API is vibing ✨' });
 });
 
+
+
 // Routes
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
+
+// metrics endpoint 
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 // Initialize database and start server
 async function start() {
